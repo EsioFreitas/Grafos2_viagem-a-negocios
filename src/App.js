0ab33d6265bd45745c34f42401b358dc72aad813
EsioFreitas <esio.gustavo@gmail.com>
2020-09-09 03:48:57 -0300

    Add map and project description

--- src/App.js
+++ src/App.js
@@ -1,24 +1,38 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Brazil from '@svg-maps/brazil';
+import { SVGMap } from 'react-svg-map';
+import 'react-svg-map/lib/index.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='bg-light' style={{ height: '100vh' }}>
+      <div className='container'>
+        <div className='text-center' style={{ color: '#353535' }}>
+          <h1 className='mb-3 pt-3'>
+            Projeto{' '}
+            <span style={{ color: '#84ba7e', fontWeight: 'bold' }}>
+              Viagem a Trabalho
+            </span>{' '}
+          </h1>
+          <p>
+            Luciana foi promovida a gerente executiva da empresa GRAFOS-LTDA o
+            qual tem filiais em vários estados do Brasil. Nessa empresa, há o
+            costume dos gerentes visitarem todas as filiais uma vez por ano. No
+            entanto, Luciana precisa saber qual é o melhor percurso, ou seja, o
+            caminho que minimize as distâncias/custos dessa viagem. Luciana irá
+            informar quais são os estados que a empresa tem filiais. O serviço
+            da aplicação será encontrar o melhor caminho (custo/benefício) que
+            ela deverá percorrer.
+          </p>
+        </div>
+        <div className='row'>
+          <div className='col'>
+            <SVGMap map={Brazil} />
+          </div>
+          <div className='col'></div>
+        </div>
+      </div>
     </div>
   );
 }
